04107eed33a0ae74711708688067ab2bcc8c8f23
Rasheed Rahman <ra1da35ma@gmail.com>
2019-09-15 15:35:03 +0100

    implement tailwindcss template

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
 
 /*
  |--------------------------------------------------------------------------
@@ -16,4 +17,6 @@ mix.js('resources/js/app.js', 'public/js')
     .options({
         processCssUrls: false,
         postCss: [ tailwindcss('tailwind.config.js') ],
-    });
+    })
+    .copyDirectory('resources/img', 'public/img');
+
